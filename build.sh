34b757b9294ca8bbbdb93871439a00f2183cbd3f
Richard Taylor <richard.taylor@mirpartners.com>
2017-09-15 10:49:35 -0700

    Updated build scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-#/bin/sh
 set -e
-mvn clean install
-mvn -f plugins/idea/pom.xml clean install
-mvn -f plugins/eclipse/pom.xml clean install
-./plugins/gradle/gradlew -b plugins/gradle/build.gradle clean assemble install
+mvn install -DskipTests=true
+mvn -f plugins/idea/pom.xml install
