4e70b29579db935f5f37f54d233c4932be379b53
Richard Taylor <richard.taylor@mirpartners.com>
2017-09-15 17:21:40 -0700

    Updates for versioning and deployment, working with maven

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 set -e
 mvn install -DskipTests=true
 mvn -f plugins/idea/pom.xml install
+./plugins/gradle/gradlew -b plugins/gradle/build.gradle assemble install
--- version.sh
+++ version.sh
@@ -2,3 +2,15 @@
 set -e
 mvn versions:set -DnewVersion=${1}
 mvn versions:commit
+
+cd plugins/eclipse
+mvn versions:set -DnewVersion=${1}
+mvn versions:commit
+
+cd ../idea
+mvn versions:set -DnewVersion=${1}
+mvn versions:commit
+
+cd ../maven
+mvn versions:set -DnewVersion=${1}
+mvn versions:commit
