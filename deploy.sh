d6a202e759c0950c5abea747570d774b62645a02
jordankomoda <>
2018-08-02 14:03:46 -0700

    Modify to use our new build server dns.

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ VERSION=$1
 
 ./version.sh $VERSION
 
-mvn deploy -Durl=http://ppbuilds.servebeer.com:8081/repository/internal/ -DrepositoryId=ppbuilds
-mvn -f plugins/idea/pom.xml deploy -Durl=http://ppbuilds.servebeer.com:8081/repository/internal/ -DrepositoryId=ppbuilds
+mvn deploy -Durl=http://teamcity.ppwsinternal.com:8081/repository/internal/ -DrepositoryId=ppbuilds
+mvn -f plugins/idea/pom.xml deploy -Durl=http://teamcity.ppwsinternal.com:8081/repository/internal/ -DrepositoryId=ppbuilds
 ./plugins/gradle/gradlew -b plugins/gradle/build.gradle assemble publish -Dversion=${VERSION}
